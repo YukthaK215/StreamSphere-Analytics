30bbbda522d6e6dbb0b391c83196f5c79aac67b4
[Yuktha K] <[kyuktha215@gm,ail.com]>
2026-07-22 18:39:21 +0530

    Use Streamlit secrets for Neon connection

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,5 @@
+import streamlit as st
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(
-        "postgresql://neondb_owner:npg_Mx7LiwWmgGI5@ep-small-water-avg7m0um-pooler.c-11.us-east-1.aws.neon.tech/neondb?sslmode=require"
-    )
+    return psycopg2.connect(st.secrets["DATABASE_URL"])
