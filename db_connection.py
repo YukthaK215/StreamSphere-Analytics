2d9eb97cbd3aec5a2363f30ab5ee85a3f3713f7d
[Yuktha K] <[kyuktha215@gm,ail.com]>
2026-07-22 18:14:31 +0530

    Update database connection to Neon PostgreSQL

--- db_connection.py
+++ db_connection.py
@@ -2,9 +2,5 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="streamsphere_db",
-        user="postgres",
-        password="Streamsphere@123",
-        port="5432"
+        "postgresql://neondb_owner:npg_Mx7LiwWmgGI5@ep-small-water-avg7m0um-pooler.c-11.us-east-1.aws.neon.tech/neondb?sslmode=require"
     )
